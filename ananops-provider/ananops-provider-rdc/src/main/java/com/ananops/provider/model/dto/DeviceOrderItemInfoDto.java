38970bfa6a25e1f52e5d1c831985624ce0f2442a
dreamflown <821981739@qq.com>
2020-01-08 17:26:26 +0800

    [update] 新添功能 1. 创建备品备件订单 2. 处理备品备件订单 3. 获取备件库所有设备

--- ananops-provider/ananops-provider-rdc/src/main/java/com/ananops/provider/model/dto/DeviceOrderItemInfoDto.java
+++ ananops-provider/ananops-provider-rdc/src/main/java/com/ananops/provider/model/dto/DeviceOrderItemInfoDto.java
@@ -1,19 +1,32 @@
 package com.ananops.provider.model.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
+import org.codehaus.jackson.annotate.JsonProperty;
 
+import java.io.Serializable;
+
+@ApiModel
 @Getter
-public class DeviceOrderItemInfoDto {
-    
+public class DeviceOrderItemInfoDto implements Serializable {
+
+    @ApiModelProperty("设备编号，自定义设备不填")
+    @JsonProperty("deviceId")
     private Long id;
-    
+
+    @ApiModelProperty("设备名称")
     private String name;
-    
+
+    @ApiModelProperty("设备类型")
     private String type;
-    
+
+    @ApiModelProperty("设备厂商")
     private String manufacture;
-    
+
+    @ApiModelProperty("设备型号")
     private String model;
-    
+
+    @ApiModelProperty("申请数量")
     private Integer count;
 }
