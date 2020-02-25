ebb3dba7e46cd7f451050cb41c0b10f53a14dee3
huqiaoqian <hpp1219@126.com>
2020-02-25 21:21:41 +0800

    mdmc mqbug

--- ananops-provider/ananops-provider-mdmc/src/main/java/com/ananops/provider/mq/producer/TaskMsgProducer.java
+++ ananops-provider/ananops-provider-mdmc/src/main/java/com/ananops/provider/mq/producer/TaskMsgProducer.java
@@ -3,6 +3,7 @@ package com.ananops.provider.mq.producer;
 import com.alibaba.fastjson.JSON;
 import com.ananops.RedisKeyUtil;
 import com.ananops.base.constant.AliyunMqTopicConstants;
+import com.ananops.base.dto.MqSendMsgDto;
 import com.ananops.base.enums.ErrorCodeEnum;
 import com.ananops.base.exception.BusinessException;
 import com.ananops.provider.model.domain.MdmcTask;
@@ -30,34 +31,43 @@ public class TaskMsgProducer {
 
         String msgBody;
         MdmcSendMsgDto mdmcSendMsgDto=new MdmcSendMsgDto();
+        MqSendMsgDto<MdmcSendMsgDto> mqSendMsgDto = new MqSendMsgDto<>();
         if((status==1 || status==6 || status==10 ||status==12)&& userId!=null){
                 //发给值机员的消息
                 mdmcSendMsgDto.setStatus(status);
                 mdmcSendMsgDto.setStatusMsg(statusMsg);
                 mdmcSendMsgDto.setTaskId(taskId);
                 mdmcSendMsgDto.setUserId(userId);
-                msgBody = JSON.toJSONString(mdmcSendMsgDto);
+                mqSendMsgDto.setUserId(userId);
+                mqSendMsgDto.setMsgBodyDto(mdmcSendMsgDto);
+                msgBody = JSON.toJSONString(mqSendMsgDto);
         }else if((status==2||status==8||status==11||status==13)&&principalId!=null){
                 //发给用户负责人的消息
                 mdmcSendMsgDto.setStatus(status);
                 mdmcSendMsgDto.setStatusMsg(statusMsg);
                 mdmcSendMsgDto.setTaskId(taskId);
                 mdmcSendMsgDto.setUserId(principalId);
-                msgBody = JSON.toJSONString(mdmcSendMsgDto);
+                mqSendMsgDto.setUserId(principalId);
+                mqSendMsgDto.setMsgBodyDto(mdmcSendMsgDto);
+                msgBody = JSON.toJSONString(mqSendMsgDto);
         }else if(status==3 && facilitatorId!=null){
                 //发给服务商业务员的消息
                 mdmcSendMsgDto.setStatus(status);
                 mdmcSendMsgDto.setStatusMsg(statusMsg);
                 mdmcSendMsgDto.setTaskId(taskId);
                 mdmcSendMsgDto.setUserId(facilitatorId);
-                msgBody = JSON.toJSONString(mdmcSendMsgDto);
+                mqSendMsgDto.setUserId(facilitatorId);
+                mqSendMsgDto.setMsgBodyDto(mdmcSendMsgDto);
+                msgBody = JSON.toJSONString(mqSendMsgDto);
         }else if((status==5 ||status==9 ||status==16 ||status==17)&&maintainerId!=null ){
                 //发给维修工程师的消息
                 mdmcSendMsgDto.setStatus(status);
                 mdmcSendMsgDto.setStatusMsg(statusMsg);
                 mdmcSendMsgDto.setTaskId(taskId);
                 mdmcSendMsgDto.setUserId(maintainerId);
-                msgBody = JSON.toJSONString(mdmcSendMsgDto);
+                mqSendMsgDto.setUserId(maintainerId);
+                mqSendMsgDto.setMsgBodyDto(mdmcSendMsgDto);
+                msgBody = JSON.toJSONString(mqSendMsgDto);
         }else {
                 //找不到发送消息的对象
                 throw new BusinessException(ErrorCodeEnum.MDMC99980006);
@@ -66,7 +76,7 @@ public class TaskMsgProducer {
         String topic = AliyunMqTopicConstants.MqTopicEnum.MDMC_TOPIC.getTopic();
         String tag = AliyunMqTopicConstants.MqTagEnum.MDMC_TASK_STATUS_CHANGED.getTag();
         String key = RedisKeyUtil.createMqKey(topic, tag, String.valueOf(taskId), msgBody);
-        log.info("发送维修工单状态改变消息，mdmcSendMsgDto={}",mdmcSendMsgDto);
+        log.info("发送维修工单状态改变消息，mqSendMsgDto={}",mqSendMsgDto);
         return new MqMessageData(msgBody, topic, tag, key);
     }
 }
