ff446e1247b16564dff7ab54f5e1e5c0fb5f3524
香甜玉米棒棒糖 <tiandilingxin@qq.com>
2020-04-20 12:45:19 +0800

    账单同步返回功能已测通
    尚需异步返回消息支持，以便保证99.99%的情况下都能修改账单支付状态。异步返回有些困难，正在努力解决

--- ananops-provider/ananops-provider-bill/src/main/java/com/ananops/provider/web/AlipayController.java
+++ ananops-provider/ananops-provider-bill/src/main/java/com/ananops/provider/web/AlipayController.java
@@ -4,6 +4,7 @@ import com.alipay.api.AlipayApiException;
 import com.ananops.provider.alipay.AlipayBean;
 import com.ananops.provider.service.AlipayService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,9 +22,9 @@ public class AlipayController {
 
     /**
      * 阿里支付
-     * @param tradeNo
+     * @param outTradeNo
      * @param subject
-     * @param amount
+     * @param totalAmount
      * @param body
      * @return
      * @throws AlipayApiException
@@ -36,5 +37,33 @@ public class AlipayController {
         alipayBean.setTotal_amount(totalAmount);
         alipayBean.setBody(body);
         return alipayService.aliPay(alipayBean);
+    }
+    @GetMapping(value = "return")
+    public String success(String charset,
+                          String out_trade_no,
+                          String method,
+                          String total_amount,
+                          String sign,
+                          String trade_no,
+                          String auth_app_id,
+                          String version,
+                          String app_id,
+                          String sign_type,
+                          String seller_id,
+                          String timestamp){
+        System.out.println("***********************");
+        System.out.println("charset"+charset);
+        System.out.println("out_trade_no"+out_trade_no);
+        System.out.println("method"+method);
+        System.out.println("total_amount"+total_amount);
+        System.out.println("sign"+sign);
+        System.out.println("trade_no"+trade_no);
+        System.out.println("auth_app_id"+auth_app_id);
+        System.out.println("version"+version);
+        System.out.println("app_id"+app_id);
+        System.out.println("sign_type"+sign_type);
+        System.out.println("seller_id"+seller_id);
+        System.out.println("timestamp"+timestamp);
+        return "已成功完成支付";
     }
 }
